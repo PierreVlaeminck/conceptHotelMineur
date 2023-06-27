2855fef7094285c4c3e643bdc228251fd9db508f
mel <121940834+MelDlv@users.noreply.github.com>
2023-06-27 14:17:09 +0200

    modif minorService

--- src/main/java/com/smplon/concepthotelmineur/service/MinorService.java
+++ src/main/java/com/smplon/concepthotelmineur/service/MinorService.java
@@ -3,19 +3,17 @@ package com.smplon.concepthotelmineur.service;
 import com.smplon.concepthotelmineur.entity.Minor;
 import com.smplon.concepthotelmineur.entity.UserProfile;
 import com.smplon.concepthotelmineur.repository.MinorRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class MinorService {
 
     private final MinorRepository minorRepository;
-
-   public MinorService(MinorRepository minorRepository) {
-       this.minorRepository = minorRepository;
-   }
 
    public Minor addMinor(Minor minor){
        return minorRepository.save(minor);
