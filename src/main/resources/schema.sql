c315dc626b35b8624e16e9d1c329b67c6c3110c0
Pierre <121941634+PierreVlaeminck@users.noreply.github.com>
2023-06-27 10:37:41 +0200

    Changement dans les fichiers SQL afin d'ajouter correctement les tables users et authorities pour Spring Security.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,21 +1,15 @@
 DROP TABLE IF EXISTS 'authorities';
 DROP TABLE IF EXISTS 'users';
 
-CREATE TABLE users
-(
-    id_u            BIGINT(20) NOT NULL,
-    username        VARCHAR(50)  NOT NULL,
-    password        VARCHAR(255) NOT NULL,
-    enable          TINYINT      NOT NULL DEFAULT 1,
-    PRIMARY KEY (id_u),
+CREATE TABLE users(
+                      username VARCHAR(50) NOT NULL,
+                      password VARCHAR(100) NOT NULL,
+                      enabled TINYINT NOT NULL DEFAULT 1,
+                      PRIMARY KEY (username)
 );
 
-CREATE TABLE authorities
-(
-    id_a      BIGINT(20) NOT NULL,
-    username  VARCHAR(50) NOT NULL,
-    authority VARCHAR(50) NOT NULL,
-    id_users  BIGINT (20) NOT NULL,
-    PRIMARY KEY (id_a),
-    FOREIGN KEY (username) REFERENCES users(username)
+CREATE TABLE authorities(
+                            username VARCHAR(50) NOT NULL,
+                            authority VARCHAR(50) NOT NULL,
+                            FOREIGN KEY (username) REFERENCES users (username)
 );
