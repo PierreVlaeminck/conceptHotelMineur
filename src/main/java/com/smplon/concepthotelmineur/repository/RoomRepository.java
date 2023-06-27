be993a50716cded46b81c3819e85a37478a55b67
mel <121940834+MelDlv@users.noreply.github.com>
2023-06-27 14:32:57 +0200

    RoomService et RoomRepository fait

--- src/main/java/com/smplon/concepthotelmineur/repository/RoomRepository.java
+++ src/main/java/com/smplon/concepthotelmineur/repository/RoomRepository.java
@@ -1,9 +1,14 @@
 package com.smplon.concepthotelmineur.repository;
 
 import com.smplon.concepthotelmineur.entity.Room;
+import com.smplon.concepthotelmineur.entity.Type;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RoomRepository extends JpaRepository<Room, Long> {
+
+    Room findRoomByType(Type type);
 }
--- src/main/java/com/smplon/concepthotelmineur/service/RoomService.java
+++ src/main/java/com/smplon/concepthotelmineur/service/RoomService.java
@@ -1,4 +1,18 @@
 package com.smplon.concepthotelmineur.service;
 
+import com.smplon.concepthotelmineur.entity.Room;
+import com.smplon.concepthotelmineur.entity.Type;
+import com.smplon.concepthotelmineur.repository.RoomRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class RoomService {
+
+    private final RoomRepository roomRepository;
+
+    public Room getRoomByType(Type type){
+        return roomRepository.findRoomByType(type);
+    }
 }
