7516c119189fe8daae983b394dda5517674508fd
Pierre <121941634+PierreVlaeminck@users.noreply.github.com>
2023-06-27 16:05:40 +0200

    Ajout des services dans Booking.

--- src/main/java/com/simplon/concepthotelmineur/repository/BookingRepository.java
+++ src/main/java/com/simplon/concepthotelmineur/repository/BookingRepository.java
@@ -1,9 +1,14 @@
 package com.simplon.concepthotelmineur.repository;
 
 import com.simplon.concepthotelmineur.entity.Booking;
+import com.simplon.concepthotelmineur.entity.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, Long> {
+
+    void deleteByIdB(Long idB);
+
+    Booking getBookingByUserProfile(UserProfile userProfile);
 }
--- src/main/java/com/simplon/concepthotelmineur/service/BookingService.java
+++ src/main/java/com/simplon/concepthotelmineur/service/BookingService.java
@@ -1,4 +1,22 @@
 package com.simplon.concepthotelmineur.service;
 
+import com.simplon.concepthotelmineur.entity.Booking;
+import com.simplon.concepthotelmineur.repository.BookingRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class BookingService {
+
+    private final BookingRepository bookingRepository;
+
+    public void createBooking (Booking booking){
+        bookingRepository.save(booking);
+    }
+
+    public void deleteBooking (Long idB){
+        bookingRepository.deleteByIdB(idB);
+    }
+
 }
