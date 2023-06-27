375b9f7ba8f4669170fb508efa6225f7115092ef
mel <121940834+MelDlv@users.noreply.github.com>
2023-06-27 14:08:29 +0200

    minorRepository et minorService fait

--- src/main/java/com/smplon/concepthotelmineur/repository/MinorRepository.java
+++ src/main/java/com/smplon/concepthotelmineur/repository/MinorRepository.java
@@ -1,7 +1,12 @@
 package com.smplon.concepthotelmineur.repository;
 
 import com.smplon.concepthotelmineur.entity.Minor;
+import com.smplon.concepthotelmineur.entity.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MinorRepository extends JpaRepository<Minor, Long> {
+
+    List<Minor> findAllByUserProfile(UserProfile userProfile);
 }
--- src/main/java/com/smplon/concepthotelmineur/service/MinorService.java
+++ src/main/java/com/smplon/concepthotelmineur/service/MinorService.java
@@ -1,4 +1,35 @@
 package com.smplon.concepthotelmineur.service;
 
+import com.smplon.concepthotelmineur.entity.Minor;
+import com.smplon.concepthotelmineur.entity.UserProfile;
+import com.smplon.concepthotelmineur.repository.MinorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class MinorService {
+
+    private final MinorRepository minorRepository;
+
+   public MinorService(MinorRepository minorRepository) {
+       this.minorRepository = minorRepository;
+   }
+
+   public Minor addMinor(Minor minor){
+       return minorRepository.save(minor);
+   }
+
+    public List<Minor> getMinorByUserProfile(UserProfile userProfile){
+        return minorRepository.findAllByUserProfile(userProfile);
+    }
+
+    public void deleteMinor(Long id){
+       minorRepository.deleteById(id);
+    }
+
+    public Minor updateMinor(Minor minor){
+       return minorRepository.save(minor);
+    }
 }
