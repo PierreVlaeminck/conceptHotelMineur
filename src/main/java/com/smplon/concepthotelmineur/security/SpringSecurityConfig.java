53f4af0e1f5234f44772fdaf3cc1b4a756dfe52c
Pierre <121941634+PierreVlaeminck@users.noreply.github.com>
2023-06-27 11:50:33 +0200

    Préparation de la class Java SpringSecurityConfig.

--- src/main/java/com/smplon/concepthotelmineur/security/SpringSecurityConfig.java
+++ src/main/java/com/smplon/concepthotelmineur/security/SpringSecurityConfig.java
@@ -1,4 +1,53 @@
 package com.smplon.concepthotelmineur.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
 public class SpringSecurityConfig {
+
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login").permitAll()
+                );
+        return http.build();
+    }
+
+    /**
+     * Creates a UserDetailsManager for managing users.
+     *
+     * @param dataSource the DataSource object used for user management
+     * @return a UserDetailsManager for managing users
+     */
+    @Bean
+    public UserDetailsManager users(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    /**
+     * Creates a PasswordEncoder for encoding passwords.
+     *
+     * @return a PasswordEncoder for encoding passwords
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 }
