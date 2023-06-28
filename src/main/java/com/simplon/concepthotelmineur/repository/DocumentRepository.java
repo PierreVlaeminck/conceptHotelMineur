725e8345ecae54a5921c0dbdb5deb8e6f1d817dc
Pierre <121941634+PierreVlaeminck@users.noreply.github.com>
2023-06-28 10:56:16 +0200

    Ajout des class Java pour la gestion des nouvelles entités repository et service.

--- src/main/java/com/simplon/concepthotelmineur/repository/DocumentRepository.java
+++ src/main/java/com/simplon/concepthotelmineur/repository/DocumentRepository.java
@@ -1,6 +1,7 @@
 package com.simplon.concepthotelmineur.repository;
 
+import com.simplon.concepthotelmineur.entity.Document;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DocumentRepository extends JpaRepository<DocumentRepository, Long> {
+public interface DocumentRepository extends JpaRepository<Document, Long> {
 }
