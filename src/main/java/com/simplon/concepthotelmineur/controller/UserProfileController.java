6f0e2f245ec7218694e3b71ea6c7f61e1ac86c2d
mel <121940834+MelDlv@users.noreply.github.com>
2023-06-28 14:56:20 +0200

    Form createUser fait

--- src/main/java/com/simplon/concepthotelmineur/controller/UserProfileController.java
+++ src/main/java/com/simplon/concepthotelmineur/controller/UserProfileController.java
@@ -1,4 +1,77 @@
 package com.simplon.concepthotelmineur.controller;
 
+import ch.qos.logback.core.model.Model;
+import com.simplon.concepthotelmineur.dto.CreateUser;
+import com.simplon.concepthotelmineur.entity.UserProfile;
+import com.simplon.concepthotelmineur.service.UserProfileService;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import java.util.Arrays;
+import java.util.Collection;
+
+@Controller
 public class UserProfileController {
+
+    private final UserProfileService userProfileService;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final UserDetailsManager userDetailsManager;
+
+    public UserProfileController(UserProfileService userProfileService, PasswordEncoder passwordEncoder, UserDetailsManager userDetailsManager) {
+        this.userProfileService = userProfileService;
+        this.passwordEncoder = passwordEncoder;
+        this.userDetailsManager = userDetailsManager;
+    }
+
+    @PostMapping("/Inscription")
+    @Transactional
+    public String createUser(@Valid @ModelAttribute("CreateUser") CreateUser createUser, BindingResult validation, Model model) {
+        if (!createUser.getPassword().equals(createUser.getConfirmPassword())) {
+            createUser.setConfirmPassword("");
+            validation.addError(new FieldError("CreateUser", "confirmPassword", "Les mots de passe ne correspondent pas."));
+        }
+        if (validation.hasErrors()) {
+            return "Inscription";
+        }
+
+        String encodedPassword = passwordEncoder.encode(createUser.getPassword());
+        Collection<? extends GrantedAuthority> roles = Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
+
+        User user = new User(createUser.getLogin(), encodedPassword, roles);
+        userDetailsManager.createUser(user);
+
+        UserProfile userProfile = new UserProfile();
+        userProfile.setUsername(createUser.getLogin());
+        userProfile.setFirstName(createUser.getFirstName());
+        userProfile.setLastName(createUser.getLastName());
+        userProfile.setMailU(createUser.getMailU());
+        userProfile.setPhone(createUser.getPhone());
+        userProfile.setCellPhoneU(createUser.getCellPhoneU());
+        userProfile.setDateOfBirthU(createUser.getDateOfBirthU());
+
+        userProfileService.addUserProfile(userProfile);
+
+        return "redirect:/";
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUserProfile(@PathVariable Long id) {
+        userProfileService.deleteUserProfile(id);
+    }
 }
--- src/main/java/com/simplon/concepthotelmineur/dto/CreateUser.java
+++ src/main/java/com/simplon/concepthotelmineur/dto/CreateUser.java
@@ -1,4 +1,64 @@
 package com.simplon.concepthotelmineur.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
 public class CreateUser {
+
+    @Getter
+    @Setter
+    @NotBlank
+    @Size(min=1, max=255)
+    private String login;
+
+    @Getter
+    @Setter
+    @NotBlank
+    @Size(min=1, max=255)
+    private String password;
+
+    @Getter
+    @Setter
+    @NotBlank
+    @Size(min=1, max=255)
+    private String confirmPassword;
+
+    @Getter
+    @Setter
+    @NotBlank
+    @Size(min=1, max=255)
+    private String lastName;
+
+    @Getter
+    @Setter
+    @NotBlank
+    @Size(min=1, max=255)
+    private String firstName;
+
+    @Getter
+    @Setter
+    @NotNull
+    private LocalDate dateOfBirthU;
+
+    @Getter
+    @Setter
+    @NotBlank
+    @Size(min=1, max=320)
+    private String mailU;
+
+    @Getter
+    @Setter
+    @NotBlank
+    @Size(min=1, max=255)
+    private String phone;
+
+    @Getter
+    @Setter
+    @Size(min=1, max=255)
+    private String cellPhoneU;
 }
