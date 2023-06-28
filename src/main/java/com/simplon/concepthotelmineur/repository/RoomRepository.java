b51c27df7388fa4f067966707d496289b375b40e
mel <121940834+MelDlv@users.noreply.github.com>
2023-06-28 16:12:35 +0200

    Ajout méthode crud pour Room

--- src/main/java/com/simplon/concepthotelmineur/repository/RoomRepository.java
+++ src/main/java/com/simplon/concepthotelmineur/repository/RoomRepository.java
@@ -1,5 +1,6 @@
 package com.simplon.concepthotelmineur.repository;
 
+import com.simplon.concepthotelmineur.entity.Hostel;
 import com.simplon.concepthotelmineur.entity.Room;
 import com.simplon.concepthotelmineur.entity.Type;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,4 +12,6 @@ import java.util.List;
 public interface RoomRepository extends JpaRepository<Room, Long> {
 
     List<Room> findRoomByType(Type type);
+
+    List<Room> findRoomByHostel(Hostel hostel);
 }
