5f29680ef38e573572d3a7323adc9f2004323d7c
Zina BREXEL <121940282+ZinaBrexel@users.noreply.github.com>
2023-06-28 15:09:44 +0200

    Add html Pages

--- src/main/java/com/simplon/concepthotelmineur/controller/WebController.java
+++ src/main/java/com/simplon/concepthotelmineur/controller/WebController.java
@@ -1,4 +1,34 @@
 package com.simplon.concepthotelmineur.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class WebController {
+    @GetMapping("/")
+    public String home() {
+        return ("index");
+    }
+    @GetMapping("/login")
+    public String login(){
+        return ("login");
+    }
+    @GetMapping("/inscription")
+    public String register(){
+        return ("userRegister");
+    }
+    @GetMapping("/reservation")
+    public String booking(){
+        return ("booking");
+    }
+
+    @GetMapping("/hotel")
+    public String hotel(){
+        return ("hostel");
+    }
+    @GetMapping("/avis_clients")
+    public String reviews(){
+        return ("reviews");
+    }
+
 }
