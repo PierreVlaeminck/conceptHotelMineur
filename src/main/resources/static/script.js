3dee5df715196bec7f8fd16f4811d6be845bb51a
Allison <121940573+Salas-Allison@users.noreply.github.com>
2023-06-29 16:57:34 +0200

    Modified Reviews

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -12,18 +12,32 @@ let marker = L.marker([latitudeInput.value, longitudeInput.value]).addTo(map);
 
 <!-- Javascript barre de prix -->
 
-// Récupérer les éléments HTML nécessaires
-const rangeInput = document.getElementById('customRange2');
+// Récupérer les éléments HTML nécessaires pour la barre de prix
+const priceRangeInput = document.getElementById('customRange2');
 const priceDisplay = document.getElementById('priceDisplay');
 
 // Définir une fonction pour mettre à jour le prix
 function updatePrice() {
-    const price = rangeInput.value * 10; // Exemple : prix = position du curseur * 10
+    const price = priceRangeInput.value * 10; // Exemple : prix = position du curseur * 10
     priceDisplay.textContent = price + '€';
 }
 
-// Ajouter un gestionnaire d'événement pour détecter les changements de position du curseur
-rangeInput.addEventListener('input', updatePrice);
+// Ajouter un gestionnaire d'événement pour détecter les changements de position du curseur pour la barre de prix
+priceRangeInput.addEventListener('input', updatePrice);
 
 // Appeler la fonction updatePrice pour afficher le prix initial
 updatePrice();
+
+<!-- Javascript barre de score -->
+
+const scoreRangeInput = document.getElementById("customRange3");
+const scoreElement = document.getElementById("score");
+
+function updateScore() {
+    const score = scoreRangeInput.value + 1;
+    scoreElement.textContent = score;
+}
+
+scoreRangeInput.addEventListener("input", updateScore);
+
+updateScore();
