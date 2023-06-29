1ed97e5c4c7fb7ec4bfc4b64ad527f7a31a2946c
Allison <121940573+Salas-Allison@users.noreply.github.com>
2023-06-29 12:07:16 +0200

    Modified Price js

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -9,3 +9,21 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 // create an initial marker
 let marker = L.marker([latitudeInput.value, longitudeInput.value]).addTo(map);
+
+<!-- Javascript barre de prix -->
+
+// Récupérer les éléments HTML nécessaires
+const rangeInput = document.getElementById('customRange2');
+const priceDisplay = document.getElementById('priceDisplay');
+
+// Définir une fonction pour mettre à jour le prix
+function updatePrice() {
+    const price = rangeInput.value * 10; // Exemple : prix = position du curseur * 10
+    priceDisplay.textContent = price + '€';
+}
+
+// Ajouter un gestionnaire d'événement pour détecter les changements de position du curseur
+rangeInput.addEventListener('input', updatePrice);
+
+// Appeler la fonction updatePrice pour afficher le prix initial
+updatePrice();
