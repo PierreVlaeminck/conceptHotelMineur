0f74d2d636c1b3c9cf19f5194a6b63d486c23add
Zina BREXEL <121940282+ZinaBrexel@users.noreply.github.com>
2023-06-30 10:31:58 +0200

    Modified Hostel Page

--- src/main/java/com/simplon/concepthotelmineur/dto/CreateUser.java
+++ src/main/java/com/simplon/concepthotelmineur/dto/CreateUser.java
@@ -7,58 +7,42 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDate;
-
+@Getter
+@Setter
 public class CreateUser {
 
-    @Getter
-    @Setter
+
     @NotBlank
     @Size(min=1, max=255)
     private String login;
 
-    @Getter
-    @Setter
     @NotBlank
     @Size(min=1, max=255)
     private String password;
 
-    @Getter
-    @Setter
     @NotBlank
     @Size(min=1, max=255)
     private String confirmPassword;
 
-    @Getter
-    @Setter
     @NotBlank
     @Size(min=1, max=255)
     private String lastName;
 
-    @Getter
-    @Setter
     @NotBlank
     @Size(min=1, max=255)
     private String firstName;
 
-    @Getter
-    @Setter
     @NotNull
     private LocalDate dateOfBirthU;
 
-    @Getter
-    @Setter
     @NotBlank
     @Size(min=1, max=320)
     private String mailU;
 
-    @Getter
-    @Setter
     @NotBlank
     @Size(min=1, max=255)
     private String phone;
 
-    @Getter
-    @Setter
     @Size(min=1, max=255)
     private String cellPhoneU;
 }
